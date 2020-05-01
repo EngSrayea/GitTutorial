2efbdfeca70f7a0115126d5d9ef20203485c1b51
EngSrayea <eng.nsrayea@gmail.com>
2020-05-01 17:20:46 +0300

    Added Map View

---
+++ GitXcode/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  GitXcode
+//
+//  Created by Eng. Norii on 5/1/20.
+//  Copyright © 2020 EngSrayea. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
